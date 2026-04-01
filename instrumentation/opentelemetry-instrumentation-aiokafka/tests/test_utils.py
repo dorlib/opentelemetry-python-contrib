8cc96938206c9cd30dbb257baec6c3b7407e4132
dorlib <dorlib318@gmail.com>
2026-04-01 15:46:06 +0300

    fix(aiokafka): use AsyncMock to fix unawaited coroutine warning

--- instrumentation/opentelemetry-instrumentation-aiokafka/tests/test_utils.py
+++ instrumentation/opentelemetry-instrumentation-aiokafka/tests/test_utils.py
@@ -256,7 +256,7 @@ class TestUtils(IsolatedAsyncioTestCase):
         extract_bootstrap_servers: mock.MagicMock,
         _enrich_getmany_poll_span: mock.MagicMock,
         _enrich_getmany_topic_span: mock.MagicMock,
-        _create_consumer_span: mock.MagicMock,
+        _create_consumer_span: mock.AsyncMock,
         extract: mock.MagicMock,
     ) -> None:
         tracer = mock.MagicMock()
